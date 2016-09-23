207a43a618f0914a166f476ca52ed6adf7ef2dfc
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2016-09-23 00:45:20 -0300

    Remove .travis - Manually commiting page

--- make.jl
+++ make.jl
@@ -4,8 +4,6 @@ makedocs()
 
 deploydocs(deps = Deps.pip("pygments", "mkdocs", "mkdocs-material", "python-markdown-math"),
   repo = "github.com/JuliaSmoothOptimizers/JuliaSmoothOptimizers.github.io.git",
-  target = ".",
   julia = "release",
   latest = "site",
-  branch = "master"
 )
