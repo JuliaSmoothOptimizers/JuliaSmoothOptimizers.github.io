b6b0e549fc2602491c1d7f2bb0118c8460d9f128
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2016-09-23 00:26:48 -0300

    Hacking

--- make.jl
+++ make.jl
@@ -4,6 +4,7 @@ makedocs()
 
 deploydocs(deps = Deps.pip("pygments", "mkdocs", "mkdocs-material", "python-markdown-math"),
   repo = "github.com/JuliaSmoothOptimizers/JuliaSmoothOptimizers.github.io.git",
+  target = ".",
   julia = "release",
   latest = "site",
   branch = "master"
