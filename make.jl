d45137e0c5c9dc302d894765d01279579f2eaa41
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2016-09-23 00:10:03 -0300

    Fix repo

--- make.jl
+++ make.jl
@@ -3,7 +3,7 @@ using Documenter
 makedocs()
 
 deploydocs(deps = Deps.pip("pygments", "mkdocs", "mkdocs-material", "python-markdown-math"),
-  repo = "github.com/JuliaSmoothOptimizers/NLPModels.jl.git",
+  repo = "github.com/JuliaSmoothOptimizers/JuliaSmoothOptimizers.github.io.git",
   julia = "release",
   latest = "site",
   branch = "master"
