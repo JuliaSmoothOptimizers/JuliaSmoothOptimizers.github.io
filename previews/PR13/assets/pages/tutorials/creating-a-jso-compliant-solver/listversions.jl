eec008d02962742dff1a01ecb22f09a9db27c1df
abelsiqueira <abelsiqueira@users.noreply.github.com>
2021-11-10 20:22:47 +0000

    Deploying to gh-preview from  @ 9c5f6e928c4ad1a65371e4a991923ffa8bea2cf1 🚀

---
+++ previews/PR13/assets/pages/tutorials/creating-a-jso-compliant-solver/listversions.jl
@@ -0,0 +1,4 @@
+# This file was generated, do not modify it. # hide
+# hideall
+  include("list-versions.jl")
+  list_versions( ["ADNLPModels", "JSOSolvers", "NLPModels", "SolverBenchmark", "SolverTools"])
---
+++ previews/PR13/assets/pages/tutorials/introduction-to-linear-operators/code/ex16.jl
@@ -0,0 +1,2 @@
+# This file was generated, do not modify it. # hide
+opA[1,1] * ones(1)
