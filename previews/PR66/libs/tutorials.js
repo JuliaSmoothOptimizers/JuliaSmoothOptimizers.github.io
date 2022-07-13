06100994599623edcda1ee5b67a775e3115b61e6
abelsiqueira <abelsiqueira@users.noreply.github.com>
2022-07-13 12:57:25 +0000

    Deploying to gh-preview from  @ 9f4c4c71cba5b08f39174db404c2d154ad879a19 🚀

--- previews/PR66/libs/tutorials.js
+++ previews/PR66/libs/tutorials.js
@@ -1,6 +1,6 @@
 const tutorialInput = document.getElementById('tutorial-input');
 const tutorialsOutput = document.getElementById('tutorials-output');
-const prepath = document.getElementById('prepath').textContent;
+const prepath = '/' + document.getElementById('prepath').textContent;
 
 active_tag = '';
 
@@ -22,9 +22,7 @@ function format_tutorial(t) {
     pkgs += format_pkg(pkg)
   }
 
-  link = t.link[0] == '/' ? prepath + t.link : t.link
-  console.log(link)
-  console.log(t.link)
+  link = t.link[0] == '/' ? prepath + t.link.slice(1) : t.link
   return `<div class="news-item">
     <a href="${link}">
       <span class="is-size-4 has-text-primary">${t.title}</span>
