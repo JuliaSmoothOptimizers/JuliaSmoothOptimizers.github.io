a1600c4ef8a502dd01ca7473129260ab100e9383
abelsiqueira <abelsiqueira@users.noreply.github.com>
2022-07-29 09:46:45 +0000

    Deploying to gh-preview from  @ 14cedb2366ccb6a96b80a2aa000fa892ef8e558f 🚀

--- previews/PR65/libs/tutorials.js
+++ previews/PR65/libs/tutorials.js
@@ -22,7 +22,15 @@ function format_tutorial(t) {
     pkgs += format_pkg(pkg)
   }
 
-  link = prepath + t.link
+  link = t.link
+  // link can be internal (/etc) or external (http://some.lnk)
+  // prepath can be "" (local and gh-pages) or "preview/PRXX" (preview)
+  // if the link is internal and it is a preview mode, we have to fix it adding a / before
+  const isInternal = t.link.slice(0,4) != 'http'
+  const hasPrepath = prepatah != ""
+  if (hasPrepath && isInternal) {
+    link = '/' + link
+  }
   console.log(link)
   console.log(t.link)
   return `<div class="news-item">
