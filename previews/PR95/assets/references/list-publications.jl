0602dce2a1958b452b338487b8e995fdb30f9e9f
tmigot <tmigot@users.noreply.github.com>
2022-09-23 19:53:50 +0000

    Deploying to gh-preview from  @ 64e7e224de2cea23dcca805dafce0bec2debbae0 🚀

--- previews/PR95/assets/references/list-publications.jl
+++ previews/PR95/assets/references/list-publications.jl
@@ -18,6 +18,7 @@ function bib_string(
     push!(V, date)
   end
   if isdoi
+    replace(link, "https://doi.org/" => "")
     push!(V, "[$link](https://doi.org/" * link * ")")
   end
   str = join(V, ", ")
